e71f0b807a4f15e79041b5afb8e388148a1aee40
Adrian39 <adrian.lovi.39@gmail.com>
2018-10-29 18:29:56 -0600

    Adrian's commit

--- app/src/main/java/edu/byui/cs246team13/swimtracker/MainActivity.java
+++ app/src/main/java/edu/byui/cs246team13/swimtracker/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Adrian's comment
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
