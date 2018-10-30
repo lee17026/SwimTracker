7b426ea82bc3665041d50a485e7f2a63c862b9b3
Tim Lee <lee17026@byui.edu>
2018-10-29 17:32:57 -0700

    week 07 team activity practicing

--- app/src/main/java/edu/byui/cs246team13/swimtracker/MainActivity.java
+++ app/src/main/java/edu/byui/cs246team13/swimtracker/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     //Adrian's comment
+    // Tim's comment line 9
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
