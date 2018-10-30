24fb0fb7bac856dc23ef0b9e6048a67a9b6270f8
Tim Lee <lee17026@byui.edu>
2018-10-29 17:17:32 -0700

    Initial commit

---
+++ app/src/main/java/edu/byui/cs246team13/swimtracker/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.byui.cs246team13.swimtracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
