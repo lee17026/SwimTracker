713f55b3cad4903c9dc36e353c0ac2bb47295425
Adrian39 <adrian.lovi.39@gmail.com>
2018-11-14 19:17:07 -0600

    Practicing DefaultSharedPreferences

--- app/src/main/java/edu/byui/cs246team13/swimtracker/MainActivity.java
+++ app/src/main/java/edu/byui/cs246team13/swimtracker/MainActivity.java
@@ -1,7 +1,10 @@
 package edu.byui.cs246team13.swimtracker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +16,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnActivity = findViewById(R.id.btnOpenSettings);
+        btnActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity();
+            }
+        });
+    }
+    private void openActivity(){
+        Intent settings = new Intent(this, SettingsActivity.class);
+        startActivity(settings);
     }
 }
